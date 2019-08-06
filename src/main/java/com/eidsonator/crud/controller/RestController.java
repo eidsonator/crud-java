a87f82e44c11393418fccfc9d4a74ace07c9d113
Todd Eidson <todd@eidson.info>
2019-08-06 18:00:21 -0400

    feat(person rest): put, post, delete

--- src/main/java/com/eidsonator/crud/controller/RestController.java
+++ src/main/java/com/eidsonator/crud/controller/RestController.java
@@ -2,10 +2,12 @@ package com.eidsonator.crud.controller;
 
 import com.eidsonator.crud.mapper.PersonMapper;
 import com.eidsonator.crud.model.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin(origins = "http://127.0.0.1:3000")
@@ -21,26 +23,31 @@ public class RestController {
         return personMapper.findAll();
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:3000")
-    @RequestMapping(method = RequestMethod.OPTIONS)
-    public Person save(@RequestBody Person person) {
-        personMapper.insert(person);
-        return new Person();
-    }
-
-    @CrossOrigin(origins = "http://127.0.0.1:3000")
-    @RequestMapping(path = "/{person}", method = RequestMethod.PUT)
-    public Person put(@PathVariable("person")long id, @RequestBody String body) {
-        Person person = personMapper.findById(id);
-        String sumpting = body;
-        personMapper.update(person);
-        return person;
-    }
-
-    @CrossOrigin(origins = "http://127.0.0.1:3000")
     @RequestMapping(path = "/{person}")
     public Person getPerson(@PathVariable("person")long person) {
         return personMapper.findById(person);
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public Person post(@RequestBody String body) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person person =  objectMapper.readValue(body, Person.class);
+        personMapper.insert(person);
+        return person;
+    }
+
+    @RequestMapping(path = "/{person}", method = RequestMethod.PUT)
+    public Person put(@PathVariable("person")long id, @RequestBody String body) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Person person =  objectMapper.readValue(body, Person.class);
+        personMapper.update(person);
+        return person;
+    }
+
+    @RequestMapping(path="/{person}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable("person")long id) {
+        personMapper.delete(id);
+        return "OK";
+    }
+
 
 }
